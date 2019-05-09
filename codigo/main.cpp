9b8f9d27772d1ccc34a5873bf74efbcdf56ca1cf
lauracah <laurach2b1213@gmail.com>
2019-05-09 17:11:34 +0100

    Crea Profesor Main

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -3,8 +3,11 @@
 #include "usuario.hpp"
 
 #include <iostream>
+using namespace std;
 
 int main(void) {
-
+    string Marcos;
+    string Perez; 
+    usuario profesor(Marcos, Perez, 0, 1); 
     return 0;
 }

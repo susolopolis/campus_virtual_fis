1616a232b4334e311cc462d436918076fd37191e
lauracah <laurach2b1213@gmail.com>
2019-05-09 16:58:19 +0100

    quitar std

--- codigo/usuario.hpp
+++ codigo/usuario.hpp
@@ -6,40 +6,40 @@
 using namespace std;
 
 typedef struct {   
-    std::string title;
+    string title;
     double mark;
-    std::string date;
-    std::string description;
+    string date;
+    string description;
 }tarea;
 
 
 class usuario {
     private:
-        std::string name_;
-        std::string last_name_; 
+        string name_;
+        string last_name_; 
         int code_;
         bool teacher_;
         
     public:
-        usuario(std::string name, std::string last_name, int code, bool teacher) :
+        usuario(string name, string last_name, int code, bool teacher) :
             name_(name),
             last_name_(last_name),
             code_(code),
             teacher_(teacher)        
         { /*code*/}
 
-        std::string get_name(void)
+       string get_name(void)
         {
             return name_;
         }
-        std::string get_last_name(void){
+        string get_last_name(void){
             return last_name_;
         }
         
-        void set_name(std::string name){
+        void set_name(string name){
             name_ = name;
         }
-        void set_last_name(std::string last_name)
+        void set_last_name(string last_name)
         {
             last_name_ = last_name; 
         }

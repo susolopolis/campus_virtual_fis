05da2409ba7cb031f364c37083df21a528776fb7
lauracah <laurach2b1213@gmail.com>
2019-05-09 17:13:38 +0100

    Profesor creado correctamente

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 int main(void) {
-    string Marcos;
-    string Perez; 
+    string Marcos="Marcos";
+    string Perez="Perez"; 
     usuario profesor(Marcos, Perez, 0, 1); 
     return 0;
 }
